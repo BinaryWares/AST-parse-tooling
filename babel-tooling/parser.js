ada96cfac17f0a368a59f9194d6f2596fd35e266
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2023-12-17 20:56:02 +0100

    feature: use babel traverse to traverse the AST

--- babel-tooling/parser.js
+++ babel-tooling/parser.js
@@ -1,29 +1,20 @@
-import {parse} from '@babel/parser';
+import { parse } from "@babel/parser";
+import _traverse from "@babel/traverse";
 
-let code = `const result = 10 + (2 * 80);`;
+const traverse = _traverse.default;
+
+let code = `const result = (10 + 2) * 80;`;
 
 let ast = parse(code);
-const astBody = ast.program.body[0];
-const astDeclaration = astBody.declarations[0];
-const astExpression = astBody.expression;
 
+let numericValues = [];
 
-
-let numericLiterals = [];
-const getNumericLiterals = (astExpression) => {
-    for (let key in astExpression) {
-        if (key === 'left' || key === 'right') {
-            if(key === 'left' && astExpression.left.type === 'NumericLiteral') {
-                numericLiterals.push(astExpression.left.value);
-            }
-            if(key === 'right' && astExpression.right.type === 'NumericLiteral') {
-                numericLiterals.push(astExpression.right.value);
-            }
-            getNumericLiterals(astExpression[key]);
-        }
+traverse(ast, {
+  enter(path) {
+    if (path.isNumericLiteral()) {
+      numericValues.push(path.node.value);
     }
-}
+  },
+});
 
-getNumericLiterals(astDeclaration?.init || astExpression);
-
-console.log(numericLiterals);
+console.log(numericValues);
