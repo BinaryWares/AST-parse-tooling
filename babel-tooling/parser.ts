e3bc2b5e4a69d56bc8cdae3800d86b9029db6c75
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2023-12-17 22:02:21 +0100

    refactor: expose a method to retrieve numericLiterals from passed code input

--- babel-tooling/parser.ts
+++ babel-tooling/parser.ts
@@ -1,18 +1,20 @@
 import { parse } from "@babel/parser";
 import traverse from "@babel/traverse";
 
-let code = `const result = (10 + 2) * 80;`;
+const getNumericLieralsInCode = (code: string): number[] => {
+  let ast = parse(code);
 
-let ast = parse(code);
+  let numericValues: number[] = [];
+  
+  traverse(ast, {
+    enter(path) {
+      if (path.isNumericLiteral()) {
+        numericValues.push(path.node.value);
+      }
+    },
+  });
 
-let numericValues: number[] = [];
+  return numericValues;
+}
 
-traverse(ast, {
-  enter(path) {
-    if (path.isNumericLiteral()) {
-      numericValues.push(path.node.value);
-    }
-  },
-});
-
-console.log(numericValues);
+export default getNumericLieralsInCode;
